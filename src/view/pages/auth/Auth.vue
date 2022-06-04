807414ed48afce5f6969dd6a9f3d58839c051735
Mulugeta Chamasa <cmulugeta.me@gmail.com>
2022-06-04 02:24:52 -0500

    first commit

---
+++ src/view/pages/auth/Auth.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="d-flex flex-column flex-root">
+    <div
+      class="login login-1 login-signin-on d-flex flex-column  flex-row-fluid"
+      id="kt_login"
+    >
+      <!--begin::Content-->
+      <KTHeader></KTHeader>
+      <div
+        class="flex-row-fluid d-flex flex-column position-relative p-7 overflow-hidden"
+      >
+        <div class="d-flex flex-column-fluid flex-center mt-30 mt-lg-0">
+          <router-view></router-view>
+        </div>
+      </div>
+      <!--end::Content-->
+    </div>
+  </div>
+</template>
+
+<!-- Load login custom page styles -->
+<style lang="scss">
+@import "@/assets/sass/pages/login/login-1.scss";
+</style>
+
+<script>
+import { mapState } from "vuex";
+import KTHeader from "@/view/layout/header/Header.vue";
+
+export default {
+  name: "auth",
+   components: {
+    KTHeader,
+  },
+  methods: {},
+  computed: {
+    ...mapState({
+      errors: state => state.auth.errors
+    }),
+    backgroundImage() {
+      return process.env.BASE_URL + "media/bg/bg-4.jpg";
+    }
+  }
+};
+</script>
---
+++ src/view/pages/auth/Login.vue
@@ -0,0 +1,193 @@
+<template>
+  <div>
+    <!--begin::Content header-->
+    <!--end::Content header-->
+
+    <!--begin::Signin-->
+    <div class="login-form login-signin bg-white p-2">
+      <div class="icon-close"><v-icon class="mdi-close" md >mdi-close</v-icon></div>
+      <div class="text-center mb-5 mb-lg-10">
+        <h3 class="login-header-title">Iniciar sesión</h3>
+        <p class="login-header-playing text-muted font-weight-semi-bold">
+         ¿Estás listo para aprender jugando?
+        </p>
+      </div>
+
+      <!--begin::Form-->
+      <div class="signin-logo">
+        <v-img src="media/logos/avlogo.png"></v-img>
+      </div>
+      <b-form class="form" @submit.stop.prevent="onSubmit">
+        <div
+          role="alert"
+          v-bind:class="{ show: errors.length }"
+          class="alert fade alert-danger"
+        >
+          <div class="alert-text" v-for="(error, i) in errors" :key="i">
+            {{ error }}
+          </div>
+        </div>
+
+        <b-form-group
+          id="example-input-group-1"
+          label=""
+          label-for="example-input-1"
+          class="pr-7 pl-7"
+        >
+          <v-text-field
+            outlined
+            label="Nombre de usuario"
+            prepend-inner-icon="mdi-account"
+            v-model="$v.form.email.$model"
+            :state="validateState('email')"
+          ></v-text-field>
+
+          <b-form-invalid-feedback id="input-1-live-feedback">
+            Email is required and a valid email address.
+          </b-form-invalid-feedback>
+        </b-form-group>
+        <b-form-group
+          id="example-input-group-2"
+          label=""
+          label-for="example-input-2"
+          class="pr-7 pl-7"
+        >
+          <v-text-field
+            outlined
+            label="Contransena"
+            prepend-inner-icon="mdi-lock"
+            v-model="$v.form.password.$model"
+            :state="validateState('password')"
+          ></v-text-field>
+          <b-form-invalid-feedback id="input-2-live-feedback">
+            Password is required.
+          </b-form-invalid-feedback>
+        </b-form-group>
+
+        <!--begin::Action-->
+        <div
+          class="form-group d-flex flex-wrap justify-content-between align-items-center pr-7 pl-7"
+        >
+          <a
+            href="#"
+            class="text-dark-60 text-hover-primary my-3 mr-2"
+            id="kt_login_forgot"
+          >
+           ¿Olvidaste tu contraseña?
+          </a>
+          <button
+            ref="kt_login_signin_submit"
+            class="login-button btn btn-success font-weight-bold px-9 py-4 my-3 font-size-3 rounded-pill"
+          >
+           Iniciar sesión en FastMind
+          </button>
+        </div>
+        <!--end::Action-->
+      </b-form>
+      <div
+      class="top-0 right-0 text-right mb-15 mb-lg-0 flex-column-auto justify-content-center pr-7 pb-3"
+      >
+        <router-link
+          class="font-weight-bold font-size-3 ml-2"
+          :to="{ name: 'register' }"
+        >
+          ¿No tienes una cuenta? Regístrate ahora
+        </router-link>
+      </div>
+      <!--end::Form-->
+    </div>
+    <!--end::Signin-->
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.spinner.spinner-right {
+  padding-right: 3.5rem !important;
+}
+</style>
+
+<script>
+import { mapState } from "vuex";
+import { LOGIN, LOGOUT } from "@/core/services/store/auth.module";
+
+import { validationMixin } from "vuelidate";
+import { email, minLength, required } from "vuelidate/lib/validators";
+
+export default {
+  mixins: [validationMixin],
+  name: "login",
+  data() {
+    return {
+      // Remove this dummy login info
+      form: {
+        email: "admin@demo.com",
+        password: "demo"
+      }
+    };
+  },
+  validations: {
+    form: {
+      email: {
+        required,
+        email
+      },
+      password: {
+        required,
+        minLength: minLength(3)
+      }
+    }
+  },
+  methods: {
+    validateState(name) {
+      const { $dirty, $error } = this.$v.form[name];
+      return $dirty ? !$error : null;
+    },
+    resetForm() {
+      this.form = {
+        email: null,
+        password: null
+      };
+
+      this.$nextTick(() => {
+        this.$v.$reset();
+      });
+    },
+    onSubmit() {
+      this.$v.form.$touch();
+      if (this.$v.form.$anyError) {
+        return;
+      }
+
+      const email = this.$v.form.email.$model;
+      const password = this.$v.form.password.$model;
+
+      // clear existing errors
+      this.$store.dispatch(LOGOUT);
+
+      // set spinner to submit button
+      const submitButton = this.$refs["kt_login_signin_submit"];
+      submitButton.classList.add("spinner", "spinner-light", "spinner-right");
+
+      // dummy delay
+      setTimeout(() => {
+        // send login request
+        this.$store
+          .dispatch(LOGIN, { email, password })
+          // go to which page after successfully login
+          .then(() => this.$router.push({ name: "dashboard" }));
+
+        submitButton.classList.remove(
+          "spinner",
+          "spinner-light",
+          "spinner-right"
+        );
+      }, 2000);
+    }
+  },
+  computed: {
+    ...mapState({
+      errors: state => state.auth.errors
+    })
+  }
+};
+</script>
